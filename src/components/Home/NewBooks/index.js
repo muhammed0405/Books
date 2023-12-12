bbd15cbbcc8265a5bdd08b30cc4a7536c306c54b
Muhammet <muxammetamatov2005@gmail.com>
2023-12-12 22:16:39 +0700

    first commit

---
+++ src/components/Home/NewBooks/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewBooks = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="newBooks">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewBooks;
